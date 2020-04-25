b07c1f13ebd3d138f08aeb3edf9df161fdfa5e87
sau-skt <saurabhchatterjee18@gmail.com>
2020-04-25 07:45:51 +0530

    Saurabh

--- app/src/main/java/appdev/com/hagoclone/welcomeactivities/LoginActivity.java
+++ app/src/main/java/appdev/com/hagoclone/welcomeactivities/LoginActivity.java
@@ -119,7 +119,6 @@ public class LoginActivity extends AppCompatActivity {
 
             }
         });
-
     }
 
     @Override
--- app/src/main/java/appdev/com/hagoclone/welcomeactivities/RegisterActivity.java
+++ app/src/main/java/appdev/com/hagoclone/welcomeactivities/RegisterActivity.java
@@ -2,17 +2,58 @@ package appdev.com.hagoclone.welcomeactivities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.icu.util.Calendar;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import appdev.com.hagoclone.R;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    //Vars for widgets
+    private EditText name,dob,gender;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        WidgetsRegister();
+        DatePick();
+    }
 
+    private DatePickerDialog picker;
+
+    private void DatePick() {
+        dob.setInputType(InputType.TYPE_NULL);
+        dob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(RegisterActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                dob.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+    }
+
+    private void WidgetsRegister() {
+        name = findViewById(R.id.regusername);
+        dob = findViewById(R.id.regdob);
+        gender = findViewById(R.id.reggender);
     }
 }
