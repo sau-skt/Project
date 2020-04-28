9f031fecd06b5ab3b0999b45fa1a95ee7e6cf85a
sau-skt <saurabhchatterjee18@gmail.com>
2020-04-28 07:16:14 +0530

    Saurabh

--- app/src/main/java/appdev/com/hagoclone/homepagefragments/GamesAndChatroomFragment.java
+++ app/src/main/java/appdev/com/hagoclone/homepagefragments/GamesAndChatroomFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,9 +16,14 @@ import appdev.com.hagoclone.R;
 
 public class GamesAndChatroomFragment extends Fragment {
 
+    private RecyclerView reminderRV;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_games_and_chatroom,container,false);
+        View view = inflater.inflate(R.layout.fragment_games_and_chatroom,container,false);
+
+        reminderRV = view.findViewById(R.id.reminderrecyclerview);
+
+        return view;
     }
 }
